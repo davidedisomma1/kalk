b67c698dee1c50790ef5b78d955978542640bbd8
Amnesys <cately96@gmail.com>
2018-03-19 16:58:01 +0100

    Update main.cpp

--- Kalk/main.cpp
+++ Kalk/main.cpp
@@ -2,11 +2,25 @@
 #include <QApplication>
 #include "Logica/punto.h"
 #include <iostream>
+#include <QLabel>
+#include<QPushButton>
+#include "Grafica/mainkalk.h"
+
+using std::cout;
+using std::endl;
 int main(int argc, char *argv[])
 {
 
 
-    Punto punto(1.3,2.0),vuoto;
-    std::cout<<vuoto.x();
+    Punto punto("ciao",2,2),vuoto;
+    cout<<punto<<endl;
+    punto.setTag("hello");
+    cout<<punto<<endl;
+    qreal ipotenusa=punto.distanzaO();
+    cout<<ipotenusa<<endl;
 
+    QApplication app(argc, argv);
+    MainKalk m;
+    m.show();
+    return app.exec();
 }
